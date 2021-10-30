3b2d1a35fe88e8cdf91bdd1d57956e0d6dc94659
2021-arevalo <93291081+2021-arevalo@users.noreply.github.com>
2021-10-30 11:20:36 -0300

    Codigo Ejercicio N°1

--- main.c
+++ main.c
@@ -1,11 +1,10 @@
-/*Ejercicio 2
-Diseñe e implemente un Firmware sobre la placa STM32F4 que haga parpadear un led con un
-periodo de 100 ms. El sistema debe permitir seleccionar uno de entre 4 de los leds disponibles
-empleando un pulsador.
-Pulsador: Selecciona LED BLUE
-Alumna: Arevalo Daiana Marisol  M.U: 01502*/
+/*Ejercicio 1
+Diseñe e implemente un Firmware sobre la placa STM32F4 - Discovery que controle
+de manera secuencial el encendido y apagado de 4 LEDs. El tiempo de encendido/apagado entre cada led
+deberá ser 300ms.
+Alumna: Arevalo Daiana Marisol   MU: 01502*/
 
-#include <stdio.h>                       //librerias:
+#include <stdio.h>
 #include <stdlib.h>
 #include "diag/Trace.h"
 #include "stm32f4xx.h"
@@ -15,6 +14,24 @@ Alumna: Arevalo Daiana Marisol  M.U: 01502*/
 
 #pragma GCC diagnostic push
 
+void SysTick_Handler(void)                 //genera solicitudes de interrupcion de forma regular
+   {
+    HAL_IncTick();
+   }
+
+void Init_GPIO_LEDS(){                          //Inicializacion de Leds
+    GPIO_InitTypeDef GPIO_InitStruct = {0};
+        __HAL_RCC_GPIOD_CLK_ENABLE();
+        __HAL_RCC_GPIOA_CLK_ENABLE();
+
+        //leds
+        GPIO_InitStruct.Pin = GPIO_PIN_12 | GPIO_PIN_13 | GPIO_PIN_14 | GPIO_PIN_15;
+        GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+        GPIO_InitStruct.Pull = GPIO_PULLDOWN;
+        GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+
+        HAL_GPIO_Init(GPIOD,&GPIO_InitStruct);
+}
 void main()
 {
         HAL_Init();
@@ -22,47 +39,23 @@ void main()
 
       while (1)
         {
-   HAL_GPIO_WritePin(GPIOD, GPIO_PIN_14, GPIO_PIN_SET);     // Led 14-RED (rojo)
-   HAL_Delay(100);  //100 ms                               //genera un tiempo en ms de interrupcion en el sistema
-   HAL_GPIO_WritePin(GPIOD, GPIO_PIN_14, GPIO_PIN_RESET);
-   HAL_Delay(100);
+    	  //Cada led cambia su estado a medida que pasan los 300 ms
 
-
-   if(HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_0) == 1){
-
-   HAL_GPIO_WritePin(GPIOD, GPIO_PIN_15, GPIO_PIN_SET);    // Led 15-BLUE (azul)
-   }
-   else{
-   HAL_GPIO_WritePin(GPIOD, GPIO_PIN_15, GPIO_PIN_RESET);
-
-   }
+    	  HAL_GPIO_TogglePin(GPIOD, GPIO_PIN_12);	// Led 12-GREEN (verde)
+    	  HAL_Delay(300);   //300 ms                //genera un tiempo en ms de interrupcion en el sistema
+    	  HAL_GPIO_TogglePin(GPIOD, GPIO_PIN_12);
+    	  HAL_GPIO_TogglePin(GPIOD, GPIO_PIN_13);	// Led 13-ORANGE (anaranjado)
+    	  HAL_Delay(300);  //300 ms
+    	  HAL_GPIO_TogglePin(GPIOD, GPIO_PIN_13);
+    	  HAL_GPIO_TogglePin(GPIOD, GPIO_PIN_14);	// Led 14-RED (rojo)
+    	  HAL_Delay(300);  //300 ms
+    	  HAL_GPIO_TogglePin(GPIOD, GPIO_PIN_14);
+    	  HAL_GPIO_TogglePin(GPIOD, GPIO_PIN_15);	// Led 15-BLUE (azul)
+    	  HAL_Delay(300);  //300 ms
+    	  HAL_GPIO_TogglePin(GPIOD, GPIO_PIN_15);
         }
 }
 
-void SysTick_Handler(void)                //genera solicitudes de interrupcion de forma regular
-{
-    HAL_IncTick();
-}
 
-void Init_GPIO_LEDS(){                         //inicializa los leds y el pulsador
-    GPIO_InitTypeDef GPIO_InitStruct = {0};
-        __HAL_RCC_GPIOD_CLK_ENABLE();
-        __HAL_RCC_GPIOA_CLK_ENABLE();
-
-        // Inicializamos los Leds
-        GPIO_InitStruct.Pin = GPIO_PIN_12 | GPIO_PIN_13 | GPIO_PIN_14 | GPIO_PIN_15;
-        GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-        GPIO_InitStruct.Pull = GPIO_PULLDOWN;
-        GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-
-        HAL_GPIO_Init(GPIOD,&GPIO_InitStruct);
-
-        // Inicializamos el Pulsador
-        GPIO_InitStruct.Pin = GPIO_PIN_0;
-        GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
-        GPIO_InitStruct.Pull = GPIO_NOPULL;
-
-        HAL_GPIO_Init(GPIOA,&GPIO_InitStruct);
-}
 
 #pragma GCC diagnostic pop
